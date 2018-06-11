7b2652ac49f6c4f35bb33889e999bf8c1b4bdde3
tarunraj95 <tarunrajkhanna@gmail.com>
2018-06-11 12:14:52 +0530

    Contact List Page designed

--- public/js/controllers/contactsCtrl.js
+++ public/js/controllers/contactsCtrl.js
@@ -1,3 +1,18 @@
-app.controller("contactsCtrl", function() {
-  console.log("controller initialized");
+app.controller("contactsCtrl", function($http, $scope, $rootScope) {
+  $http
+    .get(
+      "https://raw.githubusercontent.com/tarunraj95/Json_data/master/contacts.json"
+    )
+    .then(
+      data => {
+        $scope.contacts = data.data;
+      },
+      err => {
+        console.log("There was an error while fetching the contacts");
+      }
+    );
+
+  $scope.openContact = function(index) {
+    $rootScope.selectedContact = $scope.contacts[index];
+  };
 });
